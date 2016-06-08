b34b6ecbf0b16ba53df316dd6c9bb3d25c06dd13
Hector <hi@hectorarce.es>
2016-06-08 12:41:15 +0200

    Updates

--- js/functions.js
+++ js/functions.js
@@ -308,6 +308,7 @@ function showmapresultall() {
 }
 
 function showmapresultfail() {
+  https://{hectoruch}.cartodb.com/api/v2/sql?q=INSERT INTO user_half_earth_game (cartodb_id, the_geom, correctanswer, failanswer, points) VALUES ( , ST_SetSRID(ST_Point(-110, 43),4326,34,54))&api_key={api_key}
     $("body").append("<div id='map'></div>");
     map = new L.Map('map', {
         zoomControl: false,
@@ -353,6 +354,7 @@ function showmapresultfail() {
 
 
 function showmapresultcorrect() {
+  https://{hectoruch}.cartodb.com/api/v2/sql?q=INSERT INTO user_half_earth_game (cartodb_id, the_geom, correctanswer, failanswer, points) VALUES ( , ST_SetSRID(ST_Point(-110, 43),4326,34,54))&api_key={api_key}
     $("body").append("<div id='map'></div>");
     map = new L.Map('map', {
         zoomControl: false,
