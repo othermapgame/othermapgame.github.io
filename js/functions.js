613d405bb0a2e1447e92bbfea18ad635d6e35597
Hector <hi@hectorarce.es>
2016-06-08 10:49:44 +0200

    Updates

--- js/functions.js
+++ js/functions.js
@@ -10,8 +10,8 @@ var contador = /*many questions are complete*/
     /*Get all the information about the map, use cartodb and leaflet*/
     $.getJSON('https://hectoruch.cartodb.com/api/v2/sql?q= SELECT * FROM map_game_nature', function(data) {
         $.each(data.rows, function(key, val) {
-            questions.push(val.cartodb_id);
-            answer.push(val.cartodb_id);
+            questions.push(val.name);
+            answer.push(val.name);
         });
     });
 
@@ -89,7 +89,7 @@ function main() {
                         });
                     });
                 } else {
-                    if (questions[number] == data.cartodb_id) {
+                    if (questions[number] == data.name) {
                         scorenumber++;
                         correct.push(number);
                         questions.splice(number, 1);
