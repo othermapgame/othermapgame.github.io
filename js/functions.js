589eb76502b7be00567f359961bd94bd1150e1f9
Hector <hi@hectorarce.es>
2016-06-08 13:05:36 +0200

    Updates

--- js/functions.js
+++ js/functions.js
@@ -21,6 +21,8 @@ $.getJSON('https://hectoruch.cartodb.com/api/v2/sql?q= SELECT * FROM map_game_na
     });
 });
 
+https://hectoruch.cartodb.com/api/v2/sql?q=INSERT INTO user_half_earth_game (correctanswer,failanswer,points) VALUES (12,'value2','value3')&api_key={api_key}
+
 contador = questions.length;
 number = Math.floor((Math.random() * contador) + 0); /*get random number for questions*/
 swal({
