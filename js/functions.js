30a188564b4587cd8f02bdf457639b4c5ac72023
Hector <hi@hectorarce.es>
2016-06-09 09:50:40 +0200

    Updates

--- js/functions.js
+++ js/functions.js
@@ -17,6 +17,7 @@ $.getJSON('https://hectoruch.cartodb.com/api/v2/sql?q= SELECT * FROM map_game_na
         answer.push(val.name);
         idquestion.push(val.cartodb_id);
         idanswer.push(val.cartodb_id);
+        alert(data.moreinfo)
     });
 });
 
