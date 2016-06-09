9ad9467e90280b763ef4c8916fea424c449c68a8
Hector <hi@hectorarce.es>
2016-06-09 10:11:34 +0200

    Update

--- js/functions.js
+++ js/functions.js
@@ -20,12 +20,6 @@ $.getJSON('https://hectoruch.cartodb.com/api/v2/sql?q= SELECT * FROM map_game_na
     });
 });
 
-function dosql() {
-    alert("hello");
-    http: //hectoruch.cartodb.com/api/v2/sql?q=INSERT INTO user_half_earth_game (correctanswer, failanswer, points) VALUES ('value1', 'value2', 3)&api_key=be1f15570e60388973be3cb08edb426e8df1dfbf
-}
-
-
 contador = questions.length;
 number = Math.floor((Math.random() * contador) + 0); /*get random number for questions*/
 swal({
@@ -399,4 +393,9 @@ function closedash() {
     $(".sideBar").css("top", "-610px");
     $("#openmenu").html("Open menu");
     $("#openmenu").attr("onClick", "opendash()");
+}
+
+function dosql() {
+    alert("hello");
+    http: //hectoruch.cartodb.com/api/v2/sql?q=INSERT INTO user_half_earth_game (correctanswer, failanswer, points) VALUES ('value1', 'value2', 3)&api_key=be1f15570e60388973be3cb08edb426e8df1dfbf
 }
